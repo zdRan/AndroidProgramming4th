2320ef1bbad68bf8acdcff717bba97361bbfcb48
zdRan <zdRan@foxmail.com>
2021-12-19 14:10:35 +0800

    第一章内容

--- GeoQuiz/app/src/main/java/com/zdran/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/zdran/geoquiz/MainActivity.kt
@@ -2,11 +2,31 @@ package com.zdran.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import butterknife.BindView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_LONG).show()
+
+        }
+        //挑战练习部分
+        falseButton.setOnClickListener { view: View ->
+            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
+        }
     }
 }
