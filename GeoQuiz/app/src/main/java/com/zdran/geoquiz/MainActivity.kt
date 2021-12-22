a6ea8e16028b9bfda461ff61657939538dbae69e
zdRan <zdRan@foxmail.com>
2021-12-22 23:25:38 +0800

    添加 nextButton 的监听

--- GeoQuiz/app/src/main/java/com/zdran/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/zdran/geoquiz/MainActivity.kt
@@ -5,24 +5,57 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import butterknife.BindView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    //问题列表
+    private val questionBank = listOf(
+        Question(R.string.question_1, true),
+        Question(R.string.question_2, false),
+        Question(R.string.question_3, true),
+        Question(R.string.question_4, false),
+        Question(R.string.question_5, true)
+    )
+
+    //当前问题的下标
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //设置视图
         setContentView(R.layout.activity_main)
+
+        //绑定组件
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
+        //设置监听
         trueButton.setOnClickListener {
             Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_LONG).show()
         }
         falseButton.setOnClickListener {
             Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_LONG).show()
         }
+        nextButton.setOnClickListener {
+            //重新渲染问题
+            currentIndex = (currentIndex + 1) % questionBank.size
+            val questionResId = questionBank[currentIndex].textResId
+            questionTextView.setText(questionResId)
+        }
+
+        //视图渲染
+        //1、设置问题内容
+        val questionResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionResId)
     }
 }
