829cf35eeb1738e96542c0e4d5abdf5910b0c451
zdRan <zdRan@foxmail.com>
2021-12-26 13:44:32 +0800

    修改为 ImageButton

--- GeoQuiz/app/src/main/java/com/zdran/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/zdran/geoquiz/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import butterknife.BindView
@@ -13,8 +14,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
-    private lateinit var prevButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     //问题列表
