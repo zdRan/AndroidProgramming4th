621e006f43ec9f6bec4d051cdba1d7dcfe30a86c
zdRan <cm.zdran@foxmail.com>
2021-12-31 21:45:41 +0800

    添加第二个Activity

---
+++ GeoQuiz/app/src/main/java/com/zdran/geoquiz/CheatActivity.kt
@@ -0,0 +1,11 @@
+package com.zdran.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+    }
+}
