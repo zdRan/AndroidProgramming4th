b0f64539b917f6a5efd5cf7f576b7759d83786d7
zdRan <cm.zdran@foxmail.com>
2022-01-03 17:18:04 +0800

    显示答案

--- GeoQuiz/app/src/main/java/com/zdran/geoquiz/CheatActivity.kt
+++ GeoQuiz/app/src/main/java/com/zdran/geoquiz/CheatActivity.kt
@@ -4,15 +4,37 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import java.security.AccessControlContext
 
 //Intent extra 常量
 private const val EXTRA_ANSWER_IS_TRUE = "com.zdran.geoquiz.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
+    private var answerIsTrue = false
+
+    private lateinit var answerTextView:TextView
+    private lateinit var showAnswerButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
+        //初始化数据
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+        //绑定组件
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
+
+        //设置监听
+        showAnswerButton.setOnClickListener {
+            val answerText = when{
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            answerTextView.setText(answerText)
+        }
+
     }
 
     companion object {
