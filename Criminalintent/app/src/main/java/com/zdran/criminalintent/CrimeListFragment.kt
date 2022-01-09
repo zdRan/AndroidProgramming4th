697561f196c522d718d5fbc34fbba643a02c7ffa
zdRan <cm.zdran@foxmail.com>
2022-01-09 23:10:19 +0800

    绑定crimeListFragment

--- Criminalintent/app/src/main/java/com/zdran/criminalintent/CrimeListFragment.kt
+++ Criminalintent/app/src/main/java/com/zdran/criminalintent/CrimeListFragment.kt
@@ -2,12 +2,19 @@ package com.zdran.criminalintent
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
+    private lateinit var crimeRecyclerList: RecyclerView
+
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProvider(this)[CrimeListViewModel::class.java]
     }
@@ -15,6 +22,20 @@ class CrimeListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate: total crimes :${crimeListViewModel.crimes.size}")
+    }
+
+    //处理视图
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+
+        crimeRecyclerList = view.findViewById(R.id.crime_recycler_view)
+        // RecycleView 需要指定视图管理器
+        crimeRecyclerList.layoutManager = LinearLayoutManager(context)
+        return view
     }
 
     //伴生对象
