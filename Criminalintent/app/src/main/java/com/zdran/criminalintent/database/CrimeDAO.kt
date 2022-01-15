361caedbc4616559e2e5ecbb3ff667c8fa776d4c
zdRan <cm.zdran@foxmail.com>
2022-01-15 21:14:30 +0800

    添加详细信息保存逻辑

--- Criminalintent/app/src/main/java/com/zdran/criminalintent/database/CrimeDAO.kt
+++ Criminalintent/app/src/main/java/com/zdran/criminalintent/database/CrimeDAO.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.zdran.criminalintent.Crime
 import java.util.*
 
@@ -15,6 +16,9 @@ interface CrimeDAO {
 
     @Query("select * from crime where id = (:id)")
     fun getCrime(id: UUID): LiveData<Crime?>
+
+    @Update
+    fun updateCrime(crime: Crime)
 
     @Insert
     fun addCrime(crime: Crime)
