164bf161ebb7e0efdb8847451e881d82299dd21c
zdRan <cm.zdran@foxmail.com>
2022-01-15 20:02:45 +0800

    定义回调接口

--- Criminalintent/app/src/main/java/com/zdran/criminalintent/MainActivity.kt
+++ Criminalintent/app/src/main/java/com/zdran/criminalintent/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.zdran.criminalintent
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-
-class MainActivity : AppCompatActivity() {
+import java.util.*
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +17,16 @@ class MainActivity : AppCompatActivity() {
                 .add(R.id.fragment_container, crimeFragment)
                 .commit()
         }
+    }
+
+    /**
+     * 回调方法，用于处理 CrimeListFragment 的点击事件
+     */
+    override fun onCrimeSelected(crimeId: UUID) {
+        Log.d(TAG, "onCrimeSelected: ${crimeId.toString()}")
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, CrimeFragment())
+            .addToBackStack(null)
+            .commit()
     }
 }
