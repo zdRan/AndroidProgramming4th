277b6d200197e5fd701cb60a1cbd240dcccd6540
zdRan <cm.zdran@foxmail.com>
2022-02-19 21:49:31 +0800

    添加类

---
+++ BeatBox/app/src/main/java/com/zdran/beatbox/BeatBox.kt
@@ -0,0 +1,15 @@
+package com.zdran.beatbox
+
+import android.content.res.AssetManager
+import android.util.Log
+
+private const val TAG = "BeatBox"
+private const val SOUND_FOLDER = "sample_folder"
+
+class BeatBox(private val assetManager: AssetManager) {
+    fun loadSound(): List<String> {
+        val soundNames = assetManager.list(SOUND_FOLDER)!!
+        Log.d(TAG, "loadSound: 找到 ${soundNames.size} 个文件")
+        return soundNames.asList()
+    }
+}
