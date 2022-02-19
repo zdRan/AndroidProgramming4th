54f987a478c86d9c0fd4e268ff4061fddd1b576f
zdRan <cm.zdran@foxmail.com>
2022-02-19 21:24:58 +0800

    添加视图

--- BeatBox/app/src/main/java/com/zdran/beatbox/MainActivity.kt
+++ BeatBox/app/src/main/java/com/zdran/beatbox/MainActivity.kt
@@ -2,10 +2,43 @@ package com.zdran.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.zdran.beatbox.databinding.ActivityMainBinding
+import com.zdran.beatbox.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.recycleView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+            adapter = SoundAdapter()
+        }
+    }
+
+    private inner class SoundHolder(binding: ListItemSoundBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+    }
+
+    private inner class SoundAdapter() : RecyclerView.Adapter<SoundHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
+            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
+                layoutInflater,
+                R.layout.list_item_sound,
+                parent,
+                false
+            )
+            return SoundHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+        }
+
+        override fun getItemCount() = 0
+
     }
 }
